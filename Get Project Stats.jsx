a071236ed38738ff1f44a2c8d622bdbe2a2d7a00
JamesPerlman <jam.e.perl@gmail.com>
2020-04-06 13:46:44 -0700

    Update and rename Count Keyframes In Project.jsx to Get Project Stats.jsx

--- Get Project Stats.jsx
+++ Get Project Stats.jsx
@@ -1,6 +1,10 @@
 // To run this script after downloading it, go to File > Scripts > Run Script File, and then find and select it.
 // Made by jperl
-// Dedicated to cache_bunny who makes awesome videos
+// Dedicated to cache_bunny who makes awesome videos and requested the script
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 function keyframesInProperty(property) {
 	if (property.propertyType == PropertyType.PROPERTY) {
@@ -15,21 +19,25 @@ function keyframesInProperty(property) {
 }
 
 function main() {
-    const numKeysInProject = 0;
+    const numKeys = 0;
+    const numComps = 0;
+    const numLayers = 0;
     for (var i = 1; i <= app.project.items.length; i++) {
         var item = app.project.items[i];
         if (item.typeName != "Composition") {
             continue;
         }
+        numComps += 1;
+        numLayers += item.layers.length;
         for (var j = 1; j <= item.layers.length; j++) {
             var layer = item.layers[j];
             for (var k = 1; k <= layer.numProperties; k++) {
                 var property = layer.property(k);
-                numKeysInProject += keyframesInProperty(property);
+                numKeys += keyframesInProperty(property);
             }
         }
      }
-     alert("Wow. Your project has " + numKeysInProject + " keyframes! Amazing!");
+     alert("Your project has:\n" + numberWithCommas(numComps) + " compositions\n" + numberWithCommas(numLayers) + " layers\n" + numberWithCommas(numKeys) + " keyframes\nAmazing!");
 }
 
 main();
